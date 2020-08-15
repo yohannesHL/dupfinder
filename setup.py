3e392d587f515eba3d37b9846c17690940a0cf60
Yohannes Libanos <yhlibanos@gmail.com>
2020-08-15 22:22:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,25 @@ from distutils.core import setup
 setup(
     name='dupfinder',
     version='0.1.0',
+    description='Finds duplicate files using sha256 hashes.',
+    long_description='Finds duplicate files using sha256 hash comparisons.',
+    keywords='find duplicate files deduplicate dedup deduplication',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.8'
+    ],
     author='Yohannes Libanos',
+    license='MIT',
     packages=['dupfinder'],
     entry_points={
         'console_scripts': [
             'dupfinder = dupfinder.cli:main'
         ]
     },
-    license='MIT',
-    keywords='find duplicate files, duplicate files, remove duplicate files, deduplication'
-    description='Finds duplicate files using sha256 hash comparisons.',
-    long_description='Finds duplicate files using sha256 hash comparisons.',
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3'
-    ]
 )
